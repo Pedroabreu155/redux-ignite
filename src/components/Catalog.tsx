3bbd512fe6fd67c76e7b24d582fb97a89fa5dbec
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-06-29 16:06:40 -0300

    started to use Redux

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Catalog: React.FC = () => {
+  const catalog = useSelector(state => state);
+
+  return <h1>Catalog</h1>;
+};
+
+export default Catalog;
